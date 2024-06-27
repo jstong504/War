341466a863f9aa9a2a69f7befc675daf5a39cedc
MrTofuuu <chris.stallcup@gmail.com>
2024-06-27 14:07:28 -0500

    war game instructions

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const startButton = document.getElementById("startButton");
+
+
+// user clicks start game
+// game starts
+// hand dealt
+// figure out who wins
+// display who won
+
+
+function startGame(){
+    console.log('game started');
+}
+
+function endGame(){
+    console.log('game ended');
+}
+
 function handleClick(event){
     console.log("hello")
 }
+
+
 startButton.addEventListener("click",handleClick)
